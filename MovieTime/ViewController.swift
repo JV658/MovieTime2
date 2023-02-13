129e0219f4ae98041bd2a8a582905e745cef366e
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2023-02-13 08:42:59 -0500

    Initial Commit

---
+++ MovieTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTime
+//
+//  Created by Cambrian on 2023-02-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
